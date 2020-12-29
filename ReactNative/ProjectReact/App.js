927ad00f86ccd4d7acdee86bbfa55e4d75cd326c
RyanFirmansyah77 <fryan8068@gmail.com>
2020-12-29 21:49:37 +0700

    tugas15

--- ReactNative/ProjectReact/App.js
+++ ReactNative/ProjectReact/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {StyleSheet , Text , View } from 'react-native' ;
 
-import LoginScreen from './Tugas13/LoginScreen';
-import Register from './Tugas13/register';
-import AboutMe from './Tugas13/aboutme';
-import Counter from './lifecycle/test';
-import Hooks from './lifecycle/hooks';
-import UseEffect from './lifecycle/UseEffect';
+// import LoginScreen from './Tugas13/LoginScreen';
+// import Register from './Tugas13/register';
+// import AboutMe from './Tugas13/aboutme';
+// import Counter from './lifecycle/test';
+// import Hooks from './lifecycle/hooks';
+// import UseEffect from './lifecycle/UseEffect';
 
-
-import Note from './Tugas14/App';
+import TugasNavigation from './TugasNavigation';
+// import Note from './Tugas14/App';
 export default function App(){
   return(
     //<LoginScreen/>
@@ -18,6 +18,11 @@ export default function App(){
     // <Counter/>
     // <Hooks/>
     // <UseEffect/>
-    <Note/>
+    // <Note/>
+    // <View>
+    // <Text>hao</Text>
+    <TugasNavigation/>
+    // </View>
+    
     );
 }
